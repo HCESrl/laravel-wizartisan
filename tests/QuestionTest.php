e83b5ad792ded363acf4af0c58347b32bec822a3
fsavina <fabio.savina@gmail.com>
2019-05-28 12:15:36 +0200

    Update unit tests

--- tests/QuestionTest.php
+++ tests/QuestionTest.php
@@ -8,53 +8,55 @@ use Wizartisan\Question;
 
 class QuestionTest extends TestCase
 {
-	
-	/**
-	 * @var Question
-	 */
-	protected $question;
-	
-	
-	protected function setUp ()
-	{
-		$this->question = new Question;
-	}
-	
-	
-	public function testHideAnswer ()
-	{
-		$this->assertInstanceOf ( Question::class, $this->question->hideAnswer () );
-		$this->assertEquals ( true, $this->question->secret );
-	}
-	
-	
-	public function testWithConfirmation ()
-	{
-		$this->assertInstanceOf ( Question::class, $this->question->withConfirmation ( 'Confirm text' ) );
-		$this->assertEquals ( 'Confirm text', $this->question->confirm );
-	}
-	
-	
-	public function testMustValidate ()
-	{
-		$rule = 'required';
-		
-		$this->assertInstanceOf ( Question::class, $this->question->mustValidate ( $rule ) );
-		$this->assertInternalType ( 'array', $this->question->rules );
-		$this->assertTrue ( in_array ( $rule, $this->question->rules ) );
-	}
-	
-	
-	public function testChooseFrom ()
-	{
-		$options = [
-			'foo' => 'Foo',
-			'bar' => 'Bar',
-		];
-		
-		$this->assertInstanceOf ( Question::class, $this->question->chooseFrom ( $options ) );
-		$this->assertInternalType ( 'array', $this->question->options );
-		$this->assertEquals ( $options, $this->question->options );
-	}
-	
+
+    /**
+     * @var Question
+     */
+    protected $question;
+
+
+    protected function setUp (): void
+    {
+        parent::setUp ();
+
+        $this->question = new Question;
+    }
+
+
+    public function testHideAnswer ()
+    {
+        $this->assertInstanceOf ( Question::class, $this->question->hideAnswer () );
+        $this->assertEquals ( true, $this->question->secret );
+    }
+
+
+    public function testWithConfirmation ()
+    {
+        $this->assertInstanceOf ( Question::class, $this->question->withConfirmation ( 'Confirm text' ) );
+        $this->assertEquals ( 'Confirm text', $this->question->confirm );
+    }
+
+
+    public function testMustValidate ()
+    {
+        $rule = 'required';
+
+        $this->assertInstanceOf ( Question::class, $this->question->mustValidate ( $rule ) );
+        $this->assertIsArray ( $this->question->rules );
+        $this->assertTrue ( in_array ( $rule, $this->question->rules ) );
+    }
+
+
+    public function testChooseFrom ()
+    {
+        $options = [
+            'foo' => 'Foo',
+            'bar' => 'Bar',
+        ];
+
+        $this->assertInstanceOf ( Question::class, $this->question->chooseFrom ( $options ) );
+        $this->assertIsArray ( $this->question->options );
+        $this->assertEquals ( $options, $this->question->options );
+    }
+
 }
